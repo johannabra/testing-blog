4b6da5176598135aa38c8336ae9c63dd92249188
johannabra <johanna.branting@chasacademy.se>
2024-05-22 20:32:59 +0200

    nice log in

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -19,46 +19,73 @@ const LoginComponent = () => {
   };
 
   return (
-    <section>
+    <div>
       {userLoggedIn && <Navigate to={"/home"} replace={true} />}
-      <div>
-        <h3>Welcome back User! I love you </h3>
-      </div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            autoComplete="email"
-            required
-            value={email}
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-          />
+
+      <main className="mt-40 w-full flex self-center place-content-center place-items-center">
+        <div className="w-96 space-y-5 p-4 shadow-xl border rounded-xl">
+          <div className="text-center">
+            <div className="mt-2">
+              <h3 className="text-xl font-semibold sm:text-2xl">
+                Welcome back
+              </h3>
+            </div>
+          </div>
+          <form onSubmit={onSubmit} className="space-y-5">
+            <div>
+              <label className="text-sm text-gray-600 font-bold">Email</label>
+              <input
+                type="email"
+                autoComplete="email"
+                required
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+              />
+            </div>
+
+            <div>
+              <label className="text-sm text-gray-600 font-bold">
+                Password
+              </label>
+              <input
+                type="password"
+                autoComplete="current-password"
+                required
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+              />
+            </div>
+
+            {errorMessage && (
+              <span className="text-red-600 font-bold">{errorMessage}</span>
+            )}
+
+            <button
+              type="submit"
+              disabled={isSigningIn}
+              className={`w-full px-4 py-2 text-white font-medium rounded-lg ${
+                isSigningIn
+                  ? "bg-gray-300 cursor-not-allowed"
+                  : "bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300"
+              }`}>
+              {isSigningIn ? "Signing In..." : "Sign In"}
+            </button>
+          </form>
+          <p className="text-center text-sm">
+            Don't have an account?{" "}
+            <Link to={"/register"} className="hover:underline font-bold">
+              Sign up
+            </Link>
+          </p>
         </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            autoComplete="current-password"
-            required
-            value={password}
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-          />
-        </div>
-        {errorMessage && <span>{errorMessage}</span>}
-        <button type="submit" disabled={isSigningIn}>
-          {isSigningIn ? "Signing in.." : "Log in"}
-        </button>
-      </form>
-      <div>
-        <p>Sign up if you donn´t have an account</p>
-        <Link to={"/register"}>Sign up</Link>
-      </div>
-    </section>
+      </main>
+    </div>
   );
 };
 
--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -20,60 +20,93 @@ const RegisterComponent = () => {
   };
 
   return (
-    <section>
+    <>
       {userLoggedIn && <Navigate to={"/home"} replace={true} />}
-      <div>
-        <h3>Welcome CreateUser</h3>
-        <form onSubmit={onSubmit}>
-          <div>
-            <label>Email</label>
-            <input
-              type="email"
-              autoComplete="email"
-              required
-              value={email}
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-            />
+
+      <main className="w-full mt-40 flex self-center place-content-center place-items-center">
+        <div className="w-96 space-y-5 p-4 shadow-xl border rounded-xl">
+          <div className="text-center mb-6">
+            <div className="mt-2">
+              <h3 className="text-xl font-semibold sm:text-2xl">
+                Create a new account
+              </h3>
+            </div>
           </div>
-          <div>
-            <label>Password</label>
-            <input
+          <form onSubmit={onSubmit} className="space-y-4">
+            <div>
+              <label className="text-sm text-gray-600 font-bold">Email</label>
+              <input
+                type="email"
+                autoComplete="email"
+                required
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
+              />
+            </div>
+
+            <div>
+              <label className="text-sm text-gray-600 font-bold">
+                Password
+              </label>
+              <input
+                disabled={isRegistering}
+                type="password"
+                autoComplete="new-password"
+                required
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+              />
+            </div>
+
+            <div>
+              <label className="text-sm text-gray-600 font-bold">
+                Confirm Password
+              </label>
+              <input
+                disabled={isRegistering}
+                type="password"
+                autoComplete="off"
+                required
+                value={confirmPassword}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+              />
+            </div>
+
+            {errorMessage && (
+              <span className="text-red-600 font-bold">{errorMessage}</span>
+            )}
+
+            <button
+              type="submit"
               disabled={isRegistering}
-              type="password"
-              autoComplete="new-password"
-              required
-              value={password}
-              onChange={(event) => {
-                setPassword(event.target.value);
-              }}
-            />
-          </div>
-          <div>
-            <label>Confirm password</label>
-            <input
-              disabled={isRegistering}
-              type="password"
-              autoComplete="off"
-              required
-              value={confirmPassword}
-              onChange={(event) => {
-                setConfirmPassword(event.target.value);
-              }}
-            />
-          </div>
-          {errorMessage && <span>{errorMessage}</span>}
-          <button type="submit" disabled={isRegistering}>
-            {isRegistering ? "Registering.." : "Sign up"}
-          </button>
-        </form>
-        <div>
-          <p>Already have an account?</p>
-          <Link to={"/login"}>Continue to Login</Link>
+              className={`w-full px-4 py-2 text-white font-medium rounded-lg ${
+                isRegistering
+                  ? "bg-gray-200 cursor-not-allowed"
+                  : "bg-indigo-600 hover:bg-indigo-900 hover:shadow-xl transition duration-300"
+              }`}>
+              {isRegistering ? "Signing Up..." : "Sign Up"}
+            </button>
+            <div className="text-sm text-center">
+              Already have an account? {"   "}
+              <Link
+                to={"/login"}
+                className="text-center text-sm hover:underline font-bold">
+                Continue
+              </Link>
+            </div>
+          </form>
         </div>
-      </div>
-    </section>
+      </main>
+    </>
   );
 };
 
